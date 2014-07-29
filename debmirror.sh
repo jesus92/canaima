6907bd71a9cdeac0082be9f68455b39104fefd1f
root <root@ususario-pc.(none)>
2014-07-29 14:29:46 -0430

    subiendo repositorio

---
+++ debmirror.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+debmirror --host=http.us.debian.org --root=debian --cleanup --nosource --progress --ignore-release-gpg --arch=i386 --dist=stable --method=http --section=main,contrib,non-free /home/ususario/control/debian
